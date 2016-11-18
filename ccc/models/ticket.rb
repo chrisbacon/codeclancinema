ac3676ba9c75a69483b426cec5532964c8ecaa39
cbacon <bacon.chris@gmail.com>
2016-11-18 20:25:46 +0000

    create initialize method for ticket class

--- ccc/models/ticket.rb
+++ ccc/models/ticket.rb
@@ -1,1 +1,12 @@
 require_relative('../db/sql_runner.rb')
+
+class Ticket
+	attr_reader :customer_id, :film_id, :id
+	def initialize( options )
+		@customer_id = options['customer_id'].to_i
+		@film_id = options['film_id'].to_i
+		@id = options['id'].to_i if options['id']
+	end
+	
+	
+end
