064605a839b1fb861460d6e810cf51a4bdbbac68
cbacon <bacon.chris@gmail.com>
2016-11-18 20:38:44 +0000

    added delete and get_by_id to ticket

--- ccc/models/ticket.rb
+++ ccc/models/ticket.rb
@@ -23,5 +23,26 @@ class Ticket
 		@id = result[0]['id'].to_i 
 	end
 
+	def delete()
+		sql = "
+		DELETE FROM tickets
+		WHERE id = #{@id}
+		;"
+
+		SqlRunner.run(sql)
+	end
+
+	def self.get_by_id(id)
+		sql = "
+		SELECT * 
+		FROM tickets
+		WHERE id = #{id}
+		;"
+
+		result = SqlRunner.run(sql).first
+
+		return Ticket.new(result) if result.length>0
+	end
+
 	
 end
