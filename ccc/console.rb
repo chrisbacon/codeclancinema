b3108d5de77c62dda37823bad7d0050ea0c56fcf
cbacon <bacon.chris@gmail.com>
2016-11-18 20:36:16 +0000

    added save to ticket class

--- ccc/console.rb
+++ ccc/console.rb
@@ -1,5 +1,6 @@
 require_relative('models/customer')
 require_relative('models/film')
+require_relative('models/ticket')
 require('pry-byebug')
 
 cust1 = Customer.new({
@@ -15,6 +16,11 @@ film1 = Film.new({
 	})
 
 film1.save()
+
+ticket1 = Ticket.new({
+	'film_id' => film1.id,
+	'customer_id' => cust1.id
+	})
 
 binding.pry
 nil
--- ccc/models/ticket.rb
+++ ccc/models/ticket.rb
@@ -2,11 +2,26 @@ require_relative('../db/sql_runner.rb')
 
 class Ticket
 	attr_reader :customer_id, :film_id, :id
+
 	def initialize( options )
 		@customer_id = options['customer_id'].to_i
 		@film_id = options['film_id'].to_i
 		@id = options['id'].to_i if options['id']
 	end
 	
+	def save()
+		sql = "
+		INSERT INTO tickets
+		(customer_id, film_id)
+		VALUES
+		('#{@customer_id}', #{@film_id})
+		returning *
+		;"
+
+		result = SqlRunner.run(sql)
+
+		@id = result[0]['id'].to_i 
+	end
+
 	
 end
